6d2c29dc3f7ade1eefb3fcb1c04159a035af72c9
dima2282222 <113624834+dima2282222@users.noreply.github.com>
2023-01-24 02:25:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// let x = setInterval(Int, 1000);
+// let y = 0;
+// function Int(){
+// console.log(y);
+//     y++;
+//     if (y == 5) {
+//         clearInterval(x);
+//     }
+// }
+
+// setTimeout(function(){
+//     let x = setInterval(Int, 1000);
+//     let y = 0;
+//     function Int(){
+//     console.log(y);
+//         y++;
+//         if (y == 5) {
+//             clearInterval(x);
+//         }
+//     }
+// }, 5000);
+
+// let x = prompt ("Your name", "");
+// if (x == "Dima") {
+//     alert("Hellow");
+// }
+// else if (x == "Misha") {
+//     alert("Good Buy");
+// } else {
+//     alert("Ogooo")
+// }
+
+// $(function(){
+//     $('.rigth').slideDown(2000).slideUp(0).slideDown(2000);
+// });
+
+// $(function(){
+//     $('.left').fadeOut(0).fadeIn(2000);
+// });
+
+// function Calc(one, two){
+// console.log("One number: " + one);
+// console.log("Two number: " + two);
+// let summa = one + two;
+// console.log(summa);
+// }
+// let x = +(prompt("One Number", ''));
+// let y = +(prompt("Two Number", ''));
+// Calc(x, y)
+
+// let i = setInterval(Calc, 1000);
+// let x = 0;
+// function Calc(){
+//     ++x;
+//     console.log(x);
+//     if (x == 10) {
+//         clearInterval(i);
+//     }
+// }
